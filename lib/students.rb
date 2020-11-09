6056650e3f35987514f25b492797f44a90a4267e
Maika Kailani <queenmab051@gmail.com>
2020-11-09 22:42:02 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,6 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
+  
 end
 
 def lowest_student_gpa
@@ -13,4 +14,5 @@ def total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
+  SELECT AVG(column_name) FROM table_name;
 end
